34b120ad6ca39b9fe25e62d6df29b74ab6691edc
golden-state0fmind <areyes2009@yahoo.com>
2021-02-07 23:45:22 -0700

    finishes the Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,5 +15,51 @@ const Login = props => {
     let handleSubmit = (e) => {
         e.preventDefault()
         //space for the useModel that comes with the database
+        UserModel.login({
+            email,
+            password
+        }).then(data => {
+            if (!data.user) {
+                console.log('Login Unsuccessful')
+                return false
+            }
+            // storeUser is defined in the app component and passed to Login
+            props.storeUser(data.user)
+        })
+            .catch(err => console.log('login error', err))
     }
+    //if user is logged in, redirect
+    if (props.currentUser) return <Redirect to='/profile' />
+
+    return (
+        <div>
+            <h4>Login</h4>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label htmlFor="name">Email</label>
+                    <input
+                        onChange={handleEmail}
+                        value={email}
+                        type="email"
+                        id='email'
+                        name="email"
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        onChange={handlePassword}
+                        value={password}
+                        type="password"
+                        name="password"
+                        id="password"
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 }
+export default Login
