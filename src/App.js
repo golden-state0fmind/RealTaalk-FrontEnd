c453afcf372d5113103aee2ba1aaa14c000f2c13
golden-state0fmind <areyes2009@yahoo.com>
2021-02-07 23:46:54 -0700

    allows users to login and out based on id

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import Routes from './config/Routes'
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('id'))
+
+  const storeUser = (userId) => {
+    localStorage.setItem('id', userId)
+    setCurrentUser(userId)
+  }
+
+  const logout = (event) => {
+    event.preventDefault()
+
+    localStorage.removeItem('id')
+
+    UserModel.logout()
+      .then(res => {
+        setCurrentUser(null)
+      })
+  }
+
   return (
     <div className="App">
       <Header
         currentUser={currentUser}
         logout={logout}
+      />
+      <Routes
+        currentUser={currentUser}
+        storeUser={storeUser}
       />
       <Footer />
     </div>
