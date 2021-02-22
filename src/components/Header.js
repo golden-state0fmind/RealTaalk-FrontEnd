bd5e28985bd27731cd06edeefa19940bf4d430f6
golden-state0fmind <areyes2009@yahoo.com>
2021-02-22 01:01:40 -0700

    adds card from reactstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+    Card, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 
 const Header = (props) => {
+
     return (
-        <header>
+        <header className="header">
             <div>
-                <Link to={'/'}>Home!</Link>
-            </div>
-            <div className='links'>
-                <ul>
-                    {props.currentUser ?
-                        <>
-                            <li><Link to={'/profile'}></Link></li>
-                            <li><a href='/logout' onClick={props.logout}>Log Out</a></li>
-                        </>
-                        :
-                        <>
-                            <li><Link to={'/register'}>Register</Link></li>
-                            <li><Link to={'/login'}>Login</Link></li>
-                        </>
-                    }
-                </ul>
+                <Card>
+                    <img />
+                    <CardBody>
+                        <CardTitle tag="h5"><Link to={'/'}>Home</Link></CardTitle>
+                        <div className='links'>
+                            <ul>
+                                {props.currentUser ?
+                                    <>
+                                        <li><Link to={'/profile'}></Link></li>
+                                        <li><a href='/logout' onClick={props.logout}>Log Out</a></li>
+                                    </>
+                                    :
+                                    <>
+                                        <li><Link to={'/register'}>Register</Link></li>
+                                        <li><Link to={'/login'}>Login</Link></li>
+                                    </>
+                                }
+                            </ul>
+                        </div>
+                    </CardBody>
+                </Card>
             </div>
         </header>
     );
