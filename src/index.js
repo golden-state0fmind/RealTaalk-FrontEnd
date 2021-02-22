7f1112dd74ec5c84d3325a14ba8633c9687a1a28
golden-state0fmind <areyes2009@yahoo.com>
2021-02-21 19:58:19 -0700

    updates Router

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Router />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
