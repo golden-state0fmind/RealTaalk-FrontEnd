0a24cad70179b4bde2c5ff37930c8b8931551258
golden-state0fmind <areyes2009@yahoo.com>
2021-02-26 23:24:51 -0700

    removes url from socket io

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Col, Row, Form, Button } from "react-bootstrap";
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:4000')
+const socket = io()
 
 
 const Profile = props => {
