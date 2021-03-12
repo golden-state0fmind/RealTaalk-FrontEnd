ff654ff89941e018cad7f8a241a99142861e54f2
golden-state0fmind <areyes2009@yahoo.com>
2021-03-12 13:32:36 -0700

    changes header to NAVBAR

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-    Button, Card, CardBody, CardTitle
-} from 'reactstrap';
+import { Button, NavLink, Nav, Navbar } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 const Header = (props) => {
-    const [state, setState] = useState()
 
     return (
-        <header className="header">
-            <div>
-                <Card className="card">
-                    <CardBody className="card-body">
-                        <CardTitle tag="h5"></CardTitle>
-                        <Button className="regbtn"><Link to={'/'}>Home</Link></Button>
-                        <label className='links'>
-                            {props.currentUser ?
-                                <>
-                                    <Link to={'/profile'}></Link>
-                                    <Button className="regbtn" href='/logout' onClick={props.logout}>Log Out</Button>
-                                </>
-                                :
-                                <>
-                                    <Button className="regbtn"><Link to={'/register'}>Register</Link></Button>{' '}
-                                    <Button className="regbtn"><Link to={'/login'}>Login</Link></Button>
-                                </>
-                            }
-                        </label>
-                    </CardBody>
-                </Card>
-            </div>
-        </header>
+        <>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href='/'>Welcome to RealTaalk</Navbar.Brand>
+                <Nav justify variant="tabs">
+                    <Nav.Link><Link to={'/'}>Home</Link></Nav.Link>
+                    {props.currentUser ?
+                        <>
+                            <Nav.Link><Link to={'/profile'}></Link></Nav.Link>
+                            <Button variant="danger" href='/logout' onClick={props.logout}>Log Out</Button>
+                        </>
+                        :
+                        <>
+                            <Nav.Link><Link to={'/register'}>Register</Link></Nav.Link>
+                            <Nav.Link><Link to={'/login'}>Login</Link></Nav.Link>
+                        </>
+                    }
+                </Nav>
+            </Navbar>
+        </>
     );
 }
 export default Header;
